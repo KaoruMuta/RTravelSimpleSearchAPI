2587cc1ad0f342adbeb87ee3da6da9cc9dfe8fbe
muta <muttaroni8827@gmail.com>
2021-05-19 00:48:50 +0900

    feat: make sample get request with query parameters

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"github.com/joho/godotenv"
+	"net/http"
+	"io/ioutil"
 )
 	
 func main() {
@@ -12,7 +14,31 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to loading .env file")
 	}
-
 	applicationId := os.Getenv("APPLICATION_ID")
-	fmt.Println(applicationId)
+
+	url := "https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426"
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	params := request.URL.Query()
+	params.Set("applicationId", applicationId)
+	params.Set("largeClassCode", "japan")
+	params.Set("middleClassCode", "tokyo")
+	params.Set("smallClassCode", "tokyo")
+	params.Set("detailClassCode", "A")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(body)
 }
