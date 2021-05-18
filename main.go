f9ed25fd2e771e4c659bbc0a0f64648d5570a548
muta <muttaroni8827@gmail.com>
2021-05-19 00:19:48 +0900

    feat: create .env file and load applcation id

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+	
 func main() {
-	fmt.Println("Hello World!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to loading .env file")
+	}
+
+	applicationId := os.Getenv("APPLICATION_ID")
+	fmt.Println(applicationId)
 }
