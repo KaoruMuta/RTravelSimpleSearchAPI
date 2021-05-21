d6d4034daaba6ce6c0323404664086123a3539e3
muta <muttaroni8827@gmail.com>
2021-05-22 02:39:00 +0900

    feat: parse json response to type in go

--- main.go
+++ main.go
@@ -7,8 +7,55 @@ import (
 	"github.com/joho/godotenv"
 	"net/http"
 	"io/ioutil"
+	"encoding/json"
 )
-	
+
+type SearchResults struct {
+	PagingInfo PagingInfo `json:"pagingInfo"`
+	Hotels [][]Hotel `json:"hotels"`
+}
+
+type PagingInfo struct {
+	RecordCount int `json:"recordCount"`
+	PageCount int `json:"pageCount"`
+	Page int `json:"page"`
+	First int `json:"first"`
+	Last int `json:"last"`
+}
+
+type Hotel struct {
+	HotelBasicInfo HotelBasicInfo `json:"hotelBasicInfo"`
+	HotelRatingInfo HotelRatingInfo `json:"hotelRatingInfo"`
+	HotelDetailInfo HotelDetailInfo `json:"hotelDetailInfo"`
+	HotelFacilitiesInfo HotelFacilitiesInfo `json:"hotelFacilitiesInfo"`
+	HotelPolicyInfo HotelPolicyInfo `json:"hotelPolicyInfo"`
+	HotelOtherInfo HotelOtherInfo `json:"hotelOtherInfo"`
+}
+
+type HotelBasicInfo struct {
+	HotelNo int `json:"hotelNo"`
+}
+
+type HotelRatingInfo struct {
+	ServiceAverage float32 `json:"serviceAverage"`
+}
+
+type HotelDetailInfo struct {
+	ReserveTelephoneNo string `json:"reserveTelephoneNo"`
+}
+
+type HotelFacilitiesInfo struct {
+	HotelRoomNum int `json:"hotelRoomNum"`
+}
+
+type HotelPolicyInfo struct {
+	Note string `json:"note"`
+}
+
+type HotelOtherInfo struct {
+	Privilege string `json:"privilege"`
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -27,6 +74,7 @@ func main() {
 	params.Set("middleClassCode", "tokyo")
 	params.Set("smallClassCode", "tokyo")
 	params.Set("detailClassCode", "A")
+	params.Set("formatVersion", "2")
 
 	request.URL.RawQuery = params.Encode()
 	client := &http.Client{}
@@ -40,5 +88,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(body))
+
+	var searchResults SearchResults
+	if err := json.Unmarshal(body, &searchResults); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(searchResults.Hotels[0][0].HotelBasicInfo.HotelNo)
 }
